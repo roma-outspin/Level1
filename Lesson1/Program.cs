451b5240f8285ad0560fc0d76d36eb636f1ee38f
roma-outspin <48416418+roma-outspin@users.noreply.github.com>
2021-03-28 21:35:08 +0300

    Initial commit

---
+++ Lesson1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Последний раз спрашиваю, как тебя зовут?");
+            var userName = Console.ReadLine();
+            Console.WriteLine($"Привет, {userName}, сегодня {DateTime.Now:D}"); // строка подходящая для breakpoint, чтобы отследить значение переменной хранящей имя пользователя
+        }
+    }
+}
