24a21404f9e526975917346a6c0a8230b458b368
roma_outspin <roma_outspin@mail.ru>
2021-04-24 23:25:45 +0300

    Lesson 8

---
+++ Lesson8Lib/Worker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson8Lib
+{
+    public class Worker
+    {
+        public string Greeting()
+        {
+            return "Hello!";
+        }
+    }
+}
